23c458075d57bb744718679c6b71d1633a7f94ce
Olly Seber <olly_s@hotmail.co.uk>
2022-10-11 08:05:36 -0700

    Implement Array.my_each

--- lib/my_enumerables.rb
+++ lib/my_enumerables.rb
@@ -1,11 +1,14 @@
 module Enumerable
-  # Your code goes here
+  def my_each_with_index
+  end
 end
 
-# You will first have to define my_each
-# on the Array class. Methods defined in
-# your enumerable module will have access
-# to this method
 class Array
-  # Define my_each here
+  def my_each
+    return self unless block_given?
+
+    for e in self
+      yield(e)
+    end
+  end
 end
